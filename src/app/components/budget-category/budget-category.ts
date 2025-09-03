c693db1a9dc7fa8e21583ecdb6244f816b5fe9bb
“[Marlon <marlondossantosklain@gmail.com>
2025-09-02 22:57:33 -0300

    added inputs to the user provide what budget categories it wants

--- src/app/components/budget-category/budget-category.ts
+++ src/app/components/budget-category/budget-category.ts
@@ -2,36 +2,90 @@ import { Component, inject } from '@angular/core';
 import { CentralCard } from '../shared/central-card/central-card';
 import { Router } from '@angular/router';
 import { BudgetCategoryService } from '../../services/budget-category.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { BudgetCategory } from '../../models/budget-category.model';
+import { CommonModule } from '@angular/common';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-budget-category',
-  imports: [CentralCard],
+  imports: [CentralCard, CommonModule, ReactiveFormsModule],
   templateUrl: './budget-category.html',
   styleUrl: './budget-category.scss',
 })
 export class BudgetCategoryComponent {
-  private readonly router = inject(Router);
-  private readonly budgetCategoryService = inject(BudgetCategoryService);
   budgetCategories$!: Observable<BudgetCategory[]>;
+
+  private readonly router = inject(Router);
+  private readonly fb = inject(FormBuilder);
+  private readonly budgetCategoryService = inject(BudgetCategoryService);
+
+  budgetCategoryForm: FormGroup = this.fb.group({
+    budgetCategories: this.fb.array([]),
+  });
 
   ngOnInit(): void {
     const navigation = this.router.getCurrentNavigation();
-    this.budgetCategories$ = navigation?.extras?.state?.['budget'];
+    const budgetCategoriesState = navigation?.extras?.state?.['budget'];
 
-    if (!this.budgetCategories$) {
-      this.budgetCategoryService.getBudgetCategories().subscribe({
-        next: (response) => {
-          console.log(response);
-        },
-        error(err) {
-          console.log(err);
-        },
-        complete() {
-          console.log('COMPLETED');
-        },
-      });
+    if (!budgetCategoriesState) {
+      this.budgetCategories$ = this.budgetCategoryService.getBudgetCategories();
+    } else {
+      this.budgetCategories$ = of(budgetCategoriesState);
+    }
+
+    this.budgetCategories$.subscribe({
+      next: (defaultCategories) => {
+        const categoryFormGroups = defaultCategories.map((category) => {
+          return this.fb.group({
+            name: [category.name],
+            percentage: [category.percentage],
+          });
+        });
+
+        const categoryFormArray = this.fb.array(categoryFormGroups);
+
+        this.budgetCategoryForm.setControl(
+          'budgetCategories',
+          categoryFormArray
+        );
+      },
+      error: (error) => {
+        console.log('Error');
+      },
+      complete() {
+        console.log('DONE');
+      },
+    });
+  }
+
+  get budgetCategories(): FormArray {
+    return this.budgetCategoryForm.get('budgetCategories') as FormArray;
+  }
+
+  private newCategory(): FormGroup {
+    return this.fb.group({
+      name: [''],
+      percentage: [0],
+    });
+  }
+
+  addNewCategory(): void {
+    this.budgetCategories.push(this.newCategory());
+  }
+
+  removeCategory(index: number): void {
+    this.budgetCategories.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.budgetCategoryForm.valid) {
+      //
     }
   }
 }
